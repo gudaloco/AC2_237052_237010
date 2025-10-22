0ece00a6671427000eda0b73c2d55705cbf6991c
Gustavo <gustavo.galdino.2005@gmail.com>
2025-10-22 19:29:35 -0300

    Primeiro commit

---
+++ src/main/java/com/example/ac2_237052_237010/Ac2237052237010Application.java
@@ -0,0 +1,13 @@
+package com.example.ac2_237052_237010;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ac2237052237010Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ac2237052237010Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ac2_237052_237010/Ac2237052237010ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ac2_237052_237010;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ac2237052237010ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
